841c59f35aefcc01d0034bf2bba1ddde324deccc
Huma Taj <humataj07@gmail.com>
2026-03-06 21:08:22 +0500

    Implemented login route with session

--- webtask_3_23i0743/server.js
+++ webtask_3_23i0743/server.js
@@ -1,17 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const User = require("./User");
 
 const app = express();
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/studentDB")
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Login Systemmm");
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+
+mongoose.connect("mongodb://127.0.0.1:27017/studentDB");
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = new User(username, password);
+
+  const result = await user.register();
+
+  res.send(result);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = new User(username, password);
+
+  const result = await user.login();
+
+  if (result) {
+    req.session.user = username;
+    res.send("Login successful");
+  } else {
+    res.send("Invalid login");
+  }
 });
