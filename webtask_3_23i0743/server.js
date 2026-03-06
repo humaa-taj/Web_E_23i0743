3b45c9ed32cbe17750c842e1f6b44c8847a71968
Huma Taj <humataj07@gmail.com>
2026-03-06 21:07:15 +0500

    Added MongoDB connection using Mongoose

--- webtask_3_23i0743/server.js
+++ webtask_3_23i0743/server.js
@@ -1,11 +1,17 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/studentDB")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-    res.send("Login System")
-})
+  res.send("Login Systemmm");
+});
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000")
-})
+  console.log("Server running on port 3000");
+});
